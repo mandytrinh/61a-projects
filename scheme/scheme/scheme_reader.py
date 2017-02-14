f03f6b0d071b8d8bc0469a294aef040810dc0b02
mandytrinh <saphiric@gmail.com>
2017-02-14 09:14:05 -0800

    some more test cases for problem 2

--- scheme/scheme/scheme_reader.py
+++ scheme/scheme/scheme_reader.py
@@ -145,7 +145,7 @@ def read_tail(src):
             # END PROBLEM 1
         elif src.current() == '.':
             # BEGIN PROBLEM 2
-            "*** REPLACE THIS LINE ***"
+            while 
             # END PROBLEM 2
         else:#read the next expression recursively until the matching closing paren
             #and returns the results as a Pair instance
