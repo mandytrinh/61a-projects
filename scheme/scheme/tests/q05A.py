bc560277ac9df042dc5af4286eb6e52cfcc06d80
mandytrinh <saphiric@gmail.com>
2017-03-08 22:11:54 -0800

    more test cases

--- scheme/scheme/tests/q05A.py
+++ scheme/scheme/tests/q05A.py
@@ -13,7 +13,7 @@ test = {
   'suites': [
     [
       {
-        'answer': '4603abca9e6c9956574a67ffb7be2487',
+        'answer': 'Pair(A, Pair(B, nil)), where: A is the symbol being bound, B is an expression whose value should be bound to A',
         'choices': [
           r"""
           Pair(A, Pair(B, nil)), where:
@@ -41,19 +41,17 @@ test = {
             B is an expression whose value should be bound to A
           """
         ],
-        'locked': True,
         'question': 'What is the structure of the argument vals?',
         'type': 'concept'
       },
       {
-        'answer': 'b776d636d5ff3f2a7a941f90838c83de',
+        'answer': 'define',
         'choices': [
           'make_call_frame',
           'define',
           'lookup',
           'bindings'
         ],
-        'locked': True,
         'question': r"""
         What method of a Frame instance will bind
         a value to a symbol in that frame?
@@ -61,14 +59,12 @@ test = {
         'type': 'concept'
       },
       {
-        'locked': True,
         'test': r"""
         >>> eval('''
         ...   (define size 2)
         ...   size
         ... ''')
-        f0c1f6d92f853abf152683e4827fb490
-        # locked
+        2
         # choice: 'size'
         # choice: None
         # choice: SchemeError
@@ -77,8 +73,7 @@ test = {
         ...   (define x (+ 2 3))
         ...   x
         ... ''')
-        4c165dab3afb51604895132221bf0eb7
-        # locked
+        5
         # choice: Pair('+', Pair(2, Pair(3, nil)))
         # choice: 5
         # choice: SchemeError
